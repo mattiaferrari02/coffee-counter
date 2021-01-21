59e2f50ab98551732c62d71a3c3d780e667f173e
Mattia Ferrari <mattia2002.ferrari@gmail.com>
2021-01-21 14:04:17 +0100

    added error message

--- pages/index.js
+++ pages/index.js
@@ -5,9 +5,12 @@ import axios from "axios"
 
 
 export default function Home({counter}) {
-  const [counterUI, setCounter] = useState(counter)
+  const [counterUI, setCounter] = useState(counter);
+  const [error, setError] = useState("");
   const handleClick = async()=>{
-    await axios.get(`https://coffee-counter.vercel.app/api/add?token=${localStorage.getItem("token")}`).then(res=> {console.log(res); setCounter(counterUI+1) })
+    await axios.get(`https://coffee-counter.vercel.app/api/add?token=${localStorage.getItem("token")}`).then(res=> {console.log(res); setCounter(counterUI+1) }).catch(e=>{
+      setError(<div className="item text" >Tu non sei Mattia</div>);
+    })
   }
 
   return (
@@ -17,6 +20,7 @@ export default function Home({counter}) {
         <div className="item text" >Mattia si è bevuto</div>
         <div className="item counter" >{counterUI}☕</div>
         <div className="item text" >Caffè dal primo gennaio 2021</div>
+        {error}
         <div className="btn" onClick={handleClick} >+</div>
       </div>
     </main>
