67910473a1cfa91d1fbb3f5f88b049605c8d5298
mattiaferrari <mattia2002.ferrari@gmail.com>
2022-11-10 22:12:48 +0100

    fix deps

--- pages/api/index.ts
+++ pages/api/index.ts
@@ -1,16 +1,11 @@
 import { NextApiRequest, NextApiResponse } from "next"
-import { getCoffees } from "../../lib/redis"
 
 
 const route = async(req: NextApiRequest, res: NextApiResponse): Promise<void> => {
-  const { coffees } = await getCoffees()
 
   res.statusCode = 200
-  res.send(`Hai bevuto ${coffees} caffè`)
+  res.json({ msg: "OK" })
 }
 
-export const getData = async(): Promise<{ coffees: string }> => {
-  return getCoffees()
-}
 
 export default route
